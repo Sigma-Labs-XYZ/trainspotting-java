6cdd3b39236e98236808b8f8f11e3c4000a42567
DMcCallion <danielmccallion04@gmail.com>
2023-10-23 09:52:06 +0100

    initial commit

---
+++ src/test/java/com/example/trainspottingjava/TrainspottingJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trainspottingjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainspottingJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
