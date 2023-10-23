5a3d9a0b9217491238fac39d4d34cf9b4bf890a5
NgawangT <ngawangjb22@gmail.com>
2023-10-23 12:44:38 +0100

    added the train ID fields and getters/setters

--- src/main/java/com/example/trainspottingjava/train/Train.java
+++ src/main/java/com/example/trainspottingjava/train/Train.java
@@ -1,15 +1,20 @@
 package com.example.trainspottingjava.train;
 
 public class Train {
-
+    private int trainId;
     private String name;
     private String colour;
     private int trainNumber;
 
-    public Train(String name, String colour, int trainNumber){
+    public Train(int trainId, String name, String colour, int trainNumber){
+        this.trainId = trainId;
         this.name = name;
         this.colour = colour;
         this.trainNumber = trainNumber;
+    }
+
+    public int getTrainId(){
+        return trainId;
     }
 
     public String getName(){
@@ -26,6 +31,10 @@ public class Train {
 
     public void setName(String newName){
         this.name = newName;
+    }
+
+    public void setName(int newTrainId){
+        this.trainId = newTrainId;
     }
 
     public void setColour(String newColour){
