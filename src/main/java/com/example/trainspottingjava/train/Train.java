7e9a0c16ec81f6f602b0670e262d8ea920a1a9d6
NgawangT <ngawangjb22@gmail.com>
2023-10-23 12:34:06 +0100

    Added fields and getters + setters for Train model.

--- src/main/java/com/example/trainspottingjava/train/Train.java
+++ src/main/java/com/example/trainspottingjava/train/Train.java
@@ -1,4 +1,40 @@
 package com.example.trainspottingjava.train;
 
 public class Train {
+
+    private String name;
+    private String colour;
+    private int trainNumber;
+
+    public Train(String name, String colour, int trainNumber){
+        this.name = name;
+        this.colour = colour;
+        this.trainNumber = trainNumber;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getColour(){
+        return colour;
+    }
+
+    public int getTrainNumber(){
+        return trainNumber;
+    }
+
+    public void setName(String newName){
+        this.name = newName;
+    }
+
+    public void setColour(String newColour){
+        this.colour = newColour;
+    }
+
+    public void setTrainNumber(int newTrainNumber){
+        this.trainNumber = newTrainNumber;
+    }
 }
+
+
