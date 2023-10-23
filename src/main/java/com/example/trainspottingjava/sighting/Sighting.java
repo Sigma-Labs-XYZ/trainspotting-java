b60ea3de4ef7634b232b232cb243c96ae58a01c0
DMcCallion <danielmccallion04@gmail.com>
2023-10-23 12:25:53 +0100

    Station and sighting fields, constructors and getters and setters

--- src/main/java/com/example/trainspottingjava/sighting/Sighting.java
+++ src/main/java/com/example/trainspottingjava/sighting/Sighting.java
@@ -1,4 +1,40 @@
 package com.example.trainspottingjava.sighting;
 
+import com.example.trainspottingjava.station.Station;
+import com.example.trainspottingjava.train.Train;
+
 public class Sighting {
+    private Station station;
+    private Train train;
+    private String timestamp; //later this will be datetime object
+
+    public Sighting(Station station, Train train, String timestamp) {
+        this.station = station;
+        this.train = train;
+        this.timestamp = timestamp;
+    }
+
+    public Station getStation() {
+        return station;
+    }
+
+    public void setStation(Station station) {
+        this.station = station;
+    }
+
+    public Train getTrain() {
+        return train;
+    }
+
+    public void setTrain(Train train) {
+        this.train = train;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
 }
