7da3fff59fd22e1d17eb7682468459c7aca63783
DMcCallion <danielmccallion04@gmail.com>
2023-10-26 12:05:26 +0100

    controller's been hitting the gym

--- src/main/java/com/example/trainspottingjava/sighting/controller/SightingController.java
+++ src/main/java/com/example/trainspottingjava/sighting/controller/SightingController.java
@@ -2,6 +2,7 @@ package com.example.trainspottingjava.sighting.controller;
 
 import com.example.trainspottingjava.sighting.model.Sighting;
 import com.example.trainspottingjava.sighting.repository.SightingRepository;
+import com.example.trainspottingjava.sighting.service.SightingService;
 import com.example.trainspottingjava.station.model.Station;
 import com.example.trainspottingjava.station.repository.StationRepository;
 import com.example.trainspottingjava.train.model.Train;
@@ -27,73 +28,26 @@ public class SightingController {
         this.trainRepository = trainRepository;
     }
 
+
+
     @PostMapping("/sightings")
     public ResponseEntity<Sighting> saveSighting(@RequestBody Sighting sighting){
-        //1. If the train does not had an id, search the database for the train number. If the train does not exist, add it.
-        String trainID = sighting.getTrain().getId();
-        int trainNum = sighting.getTrain().getTrainNumber();
-        List<Train> trains = trainRepository.findAllByOrderByName().buffer().blockFirst();
 
-
-        if (trainID == null) { // If the train does not have an id:
-            //search the database for the train number.
-            Train matchingTrain = trains.stream()
-                    .filter(train -> trainNum == (train.getTrainNumber()))
-                    .findAny()
-                    .orElse(null);
-
-            //then set the ID   (for now I'm assuming the other data is correct, but some validation here would be good)
-
-            if (matchingTrain == null) { // If the train does not exist, add it.
-                String newId = this.trainRepository.save(sighting.getTrain()).block().getId();
-                sighting.getTrain().setId(newId);
-            } else {
-                sighting.getTrain().setId(matchingTrain.getId());
-            }
-        } else { //if train does have id already
-            //  TODO If I have time, we should check if that id exists in the db and if not reassign
-            //for now I'm just going to assume that if the user enters a train ID, it's a real ID and they don't typo
-            System.out.println("Post body gave train ID");
-        }
+        SightingService service = new SightingService();
+        //1. If the train does not have an id, search the database for the train number. If the train does not exist, add it.
+        service.confirmTrainID(sighting, trainRepository);
 
 
         //2. If the station does not exist, add it. (note: All UK stations have unique names)
-        String stationID = sighting.getStation().getId();
-        String stationName = sighting.getStation().getName();
-        List<Station> stations = stationRepository.findAllByOrderByName().buffer().blockFirst();
-
-
-        if (stationID == null) { // If the station does not have an id:
-            //search the database for the station name.
-            Station matchingStation = stations.stream()
-                    .filter(station -> stationName.equals(station.getName()))
-                    .findAny()
-                    .orElse(null);
-
-            //then set the ID   (for now I'm assuming the other data is correct, but some validation here would be good)
-
-            if (matchingStation == null) { // If the station does not exist, add it.
-                String newId = this.stationRepository.save(sighting.getStation()).block().getId();
-                sighting.getStation().setId(newId);
-            } else {
-                sighting.getStation().setId(matchingStation.getId());
-            }
-        } else { //if station does have id already
-            //  TODO If I have time, we should check if that id exists in the db and if not reassign
-            //for now I'm just going to assume that if the user enters a station ID, it's a real ID and they don't typo
-            System.out.println("Post body gave station ID");
-        }
+        service.confirmStationID(sighting, stationRepository);
 
         // 3. Save the sighting
-        System.out.println(sighting.getId());
-        System.out.println(sighting.getTrain().getId());
         this.sightingRepository.save(sighting).block();
 
         /*
         If there is a schema validation error we should return an error without saving anything.
         If there is an error in some of the records, we should save what we can and return a 500 error with a list of the errors
         */
-
         return new ResponseEntity<>(sighting, HttpStatus.CREATED);
     }
 }
--- src/main/java/com/example/trainspottingjava/sighting/service/SightingService.java
+++ src/main/java/com/example/trainspottingjava/sighting/service/SightingService.java
@@ -1,4 +1,75 @@
 package com.example.trainspottingjava.sighting.service;
 
+import com.example.trainspottingjava.sighting.model.Sighting;
+import com.example.trainspottingjava.station.model.Station;
+import com.example.trainspottingjava.station.repository.StationRepository;
+import com.example.trainspottingjava.train.model.Train;
+import com.example.trainspottingjava.train.repository.TrainRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class SightingService {
+
+    public void confirmTrainID(Sighting sighting, TrainRepository trainRepository){
+
+        String trainID = sighting.getTrain().getId();
+        int trainNum = sighting.getTrain().getTrainNumber();
+        List<Train> trains = trainRepository.findAllByOrderByName().buffer().blockFirst();
+
+
+        if (trainID == null) { // If the train does not have an id:
+            //search the database for the train number.
+            Train matchingTrain = trains.stream()
+                    .filter(train -> trainNum == (train.getTrainNumber()))
+                    .findAny()
+                    .orElse(null);
+
+            //then set the ID   (for now I'm assuming the other data is correct, but some validation here would be good)
+
+            if (matchingTrain == null) { // If the train does not exist, add it.
+                String newId = trainRepository.save(sighting.getTrain()).block().getId();
+                sighting.getTrain().setId(newId);
+            } else {
+                sighting.getTrain().setId(matchingTrain.getId());
+            }
+        } else { //if train does have id already
+            //  TODO If I have time, we should check if that id exists in the db and if not reassign
+            //for now I'm just going to assume that if the user enters a train ID, it's a real ID and they don't typo
+            System.out.println("Post body gave train ID");
+        }
+    }
+
+    public void confirmStationID(Sighting sighting, StationRepository stationRepository){
+
+        String stationID = sighting.getStation().getId();
+        String stationName = sighting.getStation().getName();
+        List<Station> stations = stationRepository.findAllByOrderByName().buffer().blockFirst();
+
+
+        if (stationID == null) { // If the station does not have an id:
+            //search the database for the station name.
+            Station matchingStation = stations.stream()
+                    .filter(station -> stationName.equals(station.getName()))
+                    .findAny()
+                    .orElse(null);
+
+            //then set the ID   (for now I'm assuming the other data is correct, but some validation here would be good)
+
+            if (matchingStation == null) { // If the station does not exist, add it.
+                String newId = stationRepository.save(sighting.getStation()).block().getId();
+                sighting.getStation().setId(newId);
+            } else {
+                sighting.getStation().setId(matchingStation.getId());
+            }
+        } else { //if station does have id already
+            //  TODO If I have time, we should check if that id exists in the db and if not reassign
+            //for now I'm just going to assume that if the user enters a station ID, it's a real ID and they don't typo
+            System.out.println("Post body gave station ID");
+        }
+
+    }
+
+
 }
