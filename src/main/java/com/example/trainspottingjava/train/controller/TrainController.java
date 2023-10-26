58eee2f086292ba12f2303b9cb6c4c4108bdf11e
NgawangT <ngawangjb22@gmail.com>
2023-10-26 15:44:19 +0100

    changed from plural trains to train to match the Readme doc

--- src/main/java/com/example/trainspottingjava/train/controller/TrainController.java
+++ src/main/java/com/example/trainspottingjava/train/controller/TrainController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.util.List;
 
 @RestController
-@RequestMapping("/trains")
+@RequestMapping("/train")
 public class TrainController {
 
     private final TrainRepository trainRepository;
